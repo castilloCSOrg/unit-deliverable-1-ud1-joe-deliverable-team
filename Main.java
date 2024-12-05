edfd8a425cff893dfb0beb27e7e51edd6b7718f0
Joe Carita <32655249+SamuelSeawell@users.noreply.github.com>
2024-12-05 22:48:04 +0000

    a

--- Main.java
+++ Main.java
@@ -17,41 +17,69 @@
 * <list of static methods and which teammate made each>
 *********************************************/
 
-
 public class Main 
 {
   /***** CONSTANT SECTION *****/
   static boolean spongePickedUp = false;
+  static boolean victory = false;
   public static void main(String[] args)
   {
     /***** DECLARATION SECTION *****/
 
     /***** INITIALIZATION SECTION *****/
     String currentChoice;
+    int triesNumber = 1;
     /***** INTRO SECTION *****/
 
     /***** PROCESSING SECTION *****/
 
     /***** OUTPUT SECTION *****/
-    currentChoice = UtilityBelt.readString("You arrive at a dungeon at the bottom of a great mountain. You've heard tales of the great riches within that none have laid claim to because they were lazy.\n\nAfter entering, you can go left or right. Which way would you want to go? ", 1,20);
+    intro();
+    while(victory == false)
+    {
+    currentChoice = UtilityBelt.readString("\nAfter entering, you can go left or right. Which way would you want to go? ", 1,20);
     if (currentChoice.equalsIgnoreCase("Left"))
       {
         Main.spongeEncouter();
-        if(spongePickedUp = true)
+        if(spongePickedUp == true)
         {
-          lakeEncounter(spongePickedUp);
+          lakeEncounter();
+        }
+        else
+        {
+          System.out.println("\nAs you continue deeper into the dungeon, you soon approach a large lake. Suddenly, the door locks itself behind you, and water begins to pour from the walls around you, filling the room.\nYour adventure is over.\n");
+          triesNumber++;
         }
       }
     else if (currentChoice.equalsIgnoreCase("Right"))
       {
-        System.out.println("\nGoing to the right, a door slams shut behind you. As you search for a way to escape, you find none. Your adventure is over.");
+        System.out.println("\nGoing to the right, a door slams shut behind you. As you search for a way to escape, you find none. Your adventure is over.\n");
+        triesNumber++;
       }
+    else if(currentChoice.equalsIgnoreCase("Forward"))
+    {
+      secretRoom();
+    }
     else
       {
-        System.out.println("\nThat is not a valid route, and in your indecision you fail to notice the tumbling of rocks from above you. Your adventure is over.");
+        System.out.println("\nThat is not a valid route, and in your indecision you fail to notice the tumbling of rocks from above you. Your adventure is over.\n");
+        triesNumber++;
       }
+    }
+    System.out.printf("You emerged victorious in %d tries. Congratulations!", triesNumber);
   }
   /***** STATIC METHODS *****/
+  public static void intro()
+    {
+      System.out.println("You arrive at a dungeon at the bottom of a great mountain. You've heard tales of the great riches within that none have laid claim to because of the various dangers within.");
+    }
+
+  public static void secretRoom()
+    {
+      System.out.println("\nYou find a secret room, filled to the brim with golden coins and gems! You pick up your loot and leave, successfully evading all danger!\n\n(SECRET ENDING)\n");
+      victory = true;
+    }
+
   public static void spongeEncouter()
     {
       String pickUp = UtilityBelt.readString("\nAs you keep moving forward, you find a pile of sponges along the side of the hallway. A sign on the wall states that they can absorb anything you can throw at them, which definitely\ncould be useful. Do you want to pick one up? ",1,30);
@@ -70,13 +98,21 @@ public class Main
         }
     }
 
-  public static void lakeEncounter(boolean spongeStatus)
+  public static void lakeEncounter()
     {
-      if(spongeStatus = true)
-      {
+      String useSponge = UtilityBelt.readString("As you press on, you come to a large lake, and come to the sudden realization you never learned to swim. Suddenly, the door locks itself behind you, and water begins to pour from the walls around you.\nYou remember the sponge you had just picked up. Do you wish to use it?", 1, 30);
+      if(useSponge.equalsIgnoreCase("Yes"))
+        {
+          System.out.println("You retrieve the sponge, and the moment the water touches");
+        }
+      else if(useSponge.equalsIgnoreCase("No"))
+        {
 
-      }
+        }
+      else
+        {
+
+        }
     }
 }
-
 //Keep it simple, you can build off this with loops later. For now, keep it as one path (and probably make wrong turns fatal idk.)
